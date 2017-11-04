bbb47a17e0fbea1baf92eab59c82a9a910af7a87
Rob Day <rkd@rkd.me.uk>
2017-11-04 18:05:39 +0000

    Further code cleanup

--- src/bin/datamunging.rs
+++ src/bin/datamunging.rs
@@ -18,23 +18,24 @@ impl Field {
 }
 
 fn main() {   
-    weather();
-    football();
     let max_temp = Field { start_col: 4, end_col:10};
     let min_temp = Field { start_col: 10, end_col: 14};
     let day = Field { start_col: 0, end_col: 4};
-    if let Some((spread, name)) = football2("weather.dat", max_temp, min_temp, day) {
+
+    if let Some((spread, name)) = compare_columns("weather.dat", max_temp, min_temp, day) {
         println!("Smallest spread was {} on day {}", spread, name);
     }
 
     let goals_for = Field { start_col: 44, end_col: 46};
     let goals_against = Field { start_col: 51, end_col: 53};
     let team_name = Field { start_col: 7, end_col: 23};
-    if let Some((spread, name)) = football2("football.dat", goals_for, goals_against, team_name) {
+
+    if let Some((spread, name)) = compare_columns("football.dat", goals_for, goals_against, team_name) {
         println!("Smallest spread was {} for team {}", spread, name);
     }
 }
 
+/*
 fn weather() {   
     let f = File::open("weather.dat").unwrap();
     let file = BufReader::new(&f);
@@ -80,35 +81,37 @@ fn football() {
     let team = &team_line[7..23].trim();
     println!("Smallest spread was {} on day {}", smallest_spread, team);
 }
+*/
 
-fn football2(filename: &str,
-             a_col: Field,
-             b_col: Field,
-             id_col: Field) -> Option<(f64, String)> {
+fn compare_columns(filename: &str,
+                   a_col: Field,
+                   b_col: Field,
+                   id_col: Field) -> Option<(f64, String)> {
     let f = File::open(filename).unwrap();
     let file = BufReader::new(&f);
-    let mut i = 0;
+    
+    let mut seen_headers = false;
     let mut smallest_spread = None;
     let mut chosen_line = None;
-    let mut l: String = String::new();
+    
     for line in file.lines() {
-        i += 1;
-        if i > 1 {
-            l = line.unwrap();
-            let a: f64 = a_col.extract_num(&l);
-            let b: f64 = b_col.extract_num(&l);
-            let spread = a - b;
-            if let Some(smallest_spread_val) = smallest_spread {
-                if spread < smallest_spread_val {
-                    smallest_spread = Some(spread);
-                    chosen_line = Some(l);
-                }
-            } else {
-                smallest_spread = Some(spread);
-                chosen_line = Some(l);
-            }
+        // The first line contains headers, so skip it
+        if !seen_headers {
+            seen_headers = true;
+            continue;
+        }
+
+        // Find the difference between the two columns
+        let l = line.unwrap();
+        let spread = (a_col.extract_num(&l) -  b_col.extract_num(&l)).abs();
+
+        // If this is the highest (or only) difference we've seen so far, save it off
+        if (smallest_spread.is_some() && spread < smallest_spread.unwrap()) || smallest_spread.is_none() {
+            smallest_spread = Some(spread);
+            chosen_line = Some(l);
         }
     }
+
     if let Some(line) = chosen_line {
         let team = id_col.extract_str(&line);
         return Some((smallest_spread.unwrap(), team.to_string()))
